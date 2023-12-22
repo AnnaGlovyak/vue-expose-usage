824bf39b9189a9f92b3caa146f73ec8172e77891
Анна Гловяк <anna.kmeb@gmail.com>
2023-12-22 18:16:05 +0200

    init modal examples

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from "vue";
+import Modal from "./components/script_setup/Modal.vue";
+
+const modal = ref(null);
+</script>
+
+<template>
+  <Modal />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.container {
+  display: flex;
+  width: 100%;
+  /* flex-direction: column; */
+}
+
+.left {
+  flex: 1
+}
+
+.right {
+  flex: 1
+}
+</style>
